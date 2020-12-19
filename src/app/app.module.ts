c91615e7906828c0f6541046dc581ac8167ee8d5
Dimal Gallage <dondimalgallage@gmail.com>
2020-12-19 14:28:06 +0530

    navigation with basic links added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { NavigationLinkComponent } from './components/navigation-link/navigation-link.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    NavigationLinkComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/navigation-link/navigation-link.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-link',
+  templateUrl: './navigation-link.component.html',
+  styleUrls: ['./navigation-link.component.css']
+})
+export class NavigationLinkComponent implements OnInit {
+
+  @Input() link_name:string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
